51e3d6376e56ff1159e15979e74d548a2f4ec9df
mark <ranjun@sf-express.com>
2015-05-28 17:28:08 +0800

    add projects
    new a projects for IdleMe and add lib of Alamofire

---
+++ IdleMe/IdleMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdleMe
+//
+//  Created by Administrator on 15/5/28.
+//  Copyright (c) 2015年 Mark Ran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
