ed9a142fb09572f57cd1353ccb2d47223148b517
mark <ranjun@sf-express.com>
2015-05-28 17:36:49 +0800

    add swiftJson
    add lib of json

---
+++ IdleMe/Pods/Target Support Files/Pods-IdleMe-SwiftyJSON/Pods-IdleMe-SwiftyJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IdleMe_SwiftyJSON : NSObject
+@end
+@implementation PodsDummy_Pods_IdleMe_SwiftyJSON
+@end
---
+++ IdleMe/Pods/Target Support Files/Pods-IdleMeTests-SwiftyJSON/Pods-IdleMeTests-SwiftyJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IdleMeTests_SwiftyJSON : NSObject
+@end
+@implementation PodsDummy_Pods_IdleMeTests_SwiftyJSON
+@end
