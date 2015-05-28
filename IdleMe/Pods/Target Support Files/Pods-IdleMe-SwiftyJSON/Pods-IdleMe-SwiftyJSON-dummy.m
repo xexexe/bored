47736f657e95a4b6c7b515dba7107e7183b2e080
mark <ranjun@sf-express.com>
2015-05-28 18:08:20 +0800

    del something
    again .

--- IdleMe/Pods/Target Support Files/Pods-IdleMe-SwiftyJSON/Pods-IdleMe-SwiftyJSON-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IdleMe_SwiftyJSON : NSObject
-@end
-@implementation PodsDummy_Pods_IdleMe_SwiftyJSON
-@end
--- IdleMe/Pods/Target Support Files/Pods-IdleMe/Pods-IdleMe-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IdleMe : NSObject
-@end
-@implementation PodsDummy_Pods_IdleMe
-@end
