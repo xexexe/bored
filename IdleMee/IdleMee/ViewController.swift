64d4afde25d574d70a0a745a1f85c7dc83c942e0
Mark Ran <xexexe@aliyun.com>
2015-06-01 04:11:34 +0800

    test for alamo fire lib .hehe

--- IdleMee/IdleMee/ViewController.swift
+++ IdleMee/IdleMee/ViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "http://www.ranjun.net.cn", parameters: ["foo": "bar"])
+            .response { (request, response, data, error) in
+                println(request)
+                println(response)
+                println(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
