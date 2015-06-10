454549697b954d66674d2e779c642e0d7da80ef0
mark <ranjun@sf-express.com>
2015-06-10 18:58:28 +0800

    create the basics for developer
    create a playgound for the basics of developer

---
+++ Developer/Playground/TheBasics.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+//: url 
+//: https://developer.apple.com/library/prerelease/mac/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-ID309
+
+import UIKit
+
+var str = "Hello, playground"
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
