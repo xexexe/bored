2f3fe8c195438be568718aecdee5245f129010ee
mark <ranjun@sf-express.com>
2015-06-11 17:51:51 +0800

    Constants and Variables

--- Developer/Playground/TheBasics.playground/Contents.swift
+++ Developer/Playground/TheBasics.playground/Contents.swift
@@ -9,3 +9,10 @@ var str = "Hello, playground"
 
 let maximumNumberOfLoginAttempts = 10
 var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z = 0.0
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var red, green, blue:Double
+
