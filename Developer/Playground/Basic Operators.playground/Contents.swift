adf2d695735496003f52e29011cffa629fd0977f
xexexe <xexexe@aliyun.com>
2015-06-15 17:14:26 +0800

    Basic Operator too

--- Developer/Playground/Basic Operators.playground/Contents.swift
+++ Developer/Playground/Basic Operators.playground/Contents.swift
@@ -100,7 +100,64 @@ userDefinedColorName = "green"
 
 //: Range Operators
 
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+// 1 times 5 is 5
+// 2 times 5 is 10
+// 3 times 5 is 15
+// 4 times 5 is 20
+// 5 times 5 is 25
 
+//: Half-Open Range Operator
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count {
+    print("Person \(i + 1) is called \(names[i])")
+}
+// Person 1 is called Anna
+// Person 2 is called Alex
+// Person 3 is called Brian
+// Person 4 is called Jack
+
+//: logical Operators
+let allowedEntry = false
+if !allowedEntry {
+    print("ACCESS DENIED")
+}
+// prints "ACCESS DENIED
+
+let enteredDoorCode = true
+let passedRetinaScan = false
+if enteredDoorCode && passedRetinaScan {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+// prints "ACCESS DENIED"
+
+
+let hasDoorKey = false
+let knowsOverridePassword = true
+if hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+
+if enteredDoorCode && passedRetinaScan || hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+// prints "Welcome"
+
+if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
+// prints "Welcome"
 
 
 
