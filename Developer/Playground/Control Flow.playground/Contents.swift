2085450e7b83e5ca48082b22b8a6b58f6173a894
xexexe <xexexe@aliyun.com>
2015-06-23 16:02:40 +0800

    loop while
    while.

--- Developer/Playground/Control Flow.playground/Contents.swift
+++ Developer/Playground/Control Flow.playground/Contents.swift
@@ -64,5 +64,25 @@ print("The loop statements were exeuted \(index) times")
 // prints "The loop statements were executed 3 times"
 
 //: while loop
+let finalSquare = 25
+var board = [Int](count: finalSquare, repeatedValue: 0)
+
+board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+
+var square = 0
+var diceRoll = 0
+while square < finalSquare {
+    // roll the dice
+    if ++diceRoll == 7 { diceRoll = 1 }
+    // move by the rolled amount
+    square += diceRoll
+    if square < board.count {
+        // if we're still on the board, move up or down for a snake or a ladder
+        square += board[square]
+    }
+}
+print("Game over!")
+
 
 
