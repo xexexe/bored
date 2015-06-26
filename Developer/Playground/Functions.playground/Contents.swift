0c7886978e9654c536bfff1a43cab2b210e4f25d
xexexe <xexexe@aliyun.com>
2015-06-26 17:53:46 +0800

    some change for func

--- Developer/Playground/Functions.playground/Contents.swift
+++ Developer/Playground/Functions.playground/Contents.swift
@@ -5,3 +5,87 @@
 import UIKit
 
 var str = "Functions"
+
+func sayHello(personName: String) -> String {
+    let greeting = "Hello, " + personName + "!"
+    return greeting
+}
+
+print(sayHello("Anna"))
+// prints "Hello, Anna!"
+print(sayHello("Brian"))
+// prints "Hello, Brian"
+print(sayHello("Mark"))
+// prints "Hello, Mark"
+
+func sayHelloAgain(personName: String) -> String {
+    return "Hello again, " + personName + "!"
+}
+print(sayHelloAgain("Anna"))
+// prints "Hello again, Anna!"
+
+//: Function Parameters and Return Values
+func halfOpenRangeLength(#start: Int, end: Int) -> Int {
+    return end - start
+}
+print(halfOpenRangeLength(start: 1, 10))
+// prints "9"
+print(halfOpenRangeLength(start: 2, 8))
+// prints "6"
+
+//: Functions Without Parameters
+func sayHelloWorld() -> String {
+    return "hello, world"
+}
+print(sayHelloWorld())
+// prints "hello, world"
+
+//: Functions With Mulitiple Parameters
+func sayHello(personName: String, alreadyGreeted: Bool) -> String {
+    if alreadyGreeted {
+        return sayHelloAgain(personName)
+    } else {
+        return sayHello(personName)
+    }
+}
+print(sayHello("Tim", true))
+// prints "Hello again, Tim!"
+
+//: Functions Without Return Values
+func sayGoodbye(personName: String) {
+    print("Goodbye, \(personName)!")
+}
+sayGoodbye("Dave")
+// prints "Goodbye, Dave!"
+
+func printAndCount(stringToPrint: String) -> Int {
+    print(stringToPrint)
+    return count(stringToPrint)
+}
+func printWithoutCounting(stringToPrint: String) {
+    printAndCount(stringToPrint)
+}
+printWithoutCounting("hello, world")
+// prints "hello, world" and returns a value of 12
+printWithoutCounting("hello, world")
+// prints "hello, world" but does not return a value
+
+//: Functions wiht Mulitple Return Values
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax([8, -6, 2, 109, 3, 71])
+print("min is \(bounds.min) and max is \(bounds.max)")
+// prints "min is -6 and max is 109"
+
+
