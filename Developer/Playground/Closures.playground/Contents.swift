6d83505fc2efe5be54c87bd11da42df263042d5d
xexexe <xexexe@aliyun.com>
2015-07-01 16:01:15 +0800

    for Enumerations

--- Developer/Playground/Closures.playground/Contents.swift
+++ Developer/Playground/Closures.playground/Contents.swift
@@ -52,13 +52,43 @@ let strings = numbers.map {
     (var number) -> String in
     var output = ""
     while number > 0 {
-        output = digitNames[numbers % 10]! + output
+        output = digitNames[number%10]! + output
         number /= 10
     }
     return output
 }
 // strings is inferred to be of type [String]
 // its value is ["OneSix", "FiveEight", "FiveOneZero"]
+
+//: Capturing Values
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    return incrementer
+}
+let incrementByTen = makeIncrementer(forIncrement: 10)
+incrementByTen()
+// returns a value of 10
+incrementByTen()
+// returns a value of 20
+incrementByTen()
+// returns a value of 30
+let incrementBySeven = makeIncrementer(forIncrement: 7)
+incrementBySeven()
+// returns a value of 7
+incrementByTen()
+// returns a value of 40
+
+//: Closures Are Reference Types
+let alsoIncrementByTen = incrementByTen
+alsoIncrementByTen()
+// returns a value of 50
+
+
+
 
 
 
---
+++ Developer/Playground/Enumerations.playground/Contents.swift
@@ -0,0 +1,97 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum SomeEnumeration {
+    // enumeration definition goes here
+}
+
+enum CompassPoint {
+    case North
+    case South
+    case East
+    case West
+}
+
+enum Planet {
+    case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+
+var directionToHead = CompassPoint.West
+directionToHead = .East
+
+//: Matching Enumeration Values with a Switch Statement
+directionToHead = .South
+switch directionToHead {
+case .North:
+    println("Lots of planets have a north")
+case .South:
+    println("Watch out for penguins")
+case .East:
+    println("Where the sun rises")
+case .West:
+    println("Where the skies are blue")
+}
+// prints "Watch out for penguins"
+
+let somePlanet = Planet.Earth
+switch somePlanet {
+case .Earth:
+    println("Mostly harmless")
+default:
+    println("Not a safe place for humans")
+}
+// prints "Mostly harmless"
+
+//: Associated Values
+enum Barcode {
+    case UPCA(Int, Int, Int, Int)
+    case QRCode(String)
+}
+var productBarcode = Barcode.UPCA(8, 85909, 51226, 3)
+productBarcode = .QRCode("ABCDEFGHIJKLMNOP")
+switch productBarcode {
+case .UPCA(let numberSystem, let manufacturer, let product, let check):
+    println("UPC-A: \(numberSystem), \(manufacturer), \(product), \(check).")
+case .QRCode(let productCode):
+    println("QR code: \(productCode).")
+}
+// prints "QR code: ABCDEFGHIJKLMNOP."
+
+switch productBarcode {
+case let .UPCA(numberSystem, manufacturer, product, check):
+    println("UPC-A: \(numberSystem), \(manufacturer), \(product), \(check)")
+case let .QRCode(productCode):
+    println("QR code: \(productCode)")
+}
+// prints "QR code: ABCDEFGHIJKLMNOP."
+
+//: Raw Values
+enum ASCIIControlCharacter: Character {
+    case Tab = "\t"
+    case LineFeed = "\n"
+    case CarriageReturn = "\r"
+}
+
+enum Planet2: Int {
+    case Mercury = 1, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+let earthsOrder = Planet2.Earth.rawValue
+// earthsOrder is 3
+
+//: Initializing from a Row Value
+let possiblePlanet = Planet2(rawValue: 7)
+// possiblePlanet is of type Planet? and equals Planet.Uranus
+
+let positionToFind = 9
+if let somePlanet = Planet2(rawValue: positionToFind) {
+    switch somePlanet {
+    case .Earth:
+        println("Mostly harmless")
+    default:
+        println("Not a safe place for humans")
+    }
+} else {
+    println("There isn't a planet at position \(positionToFind)")
+}
+// prints "There isn't a planet at position 9"
