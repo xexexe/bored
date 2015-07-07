85c2c0448eeb8467457bd5e88d398a2b11a4638c
xexexe <xexexe@aliyun.com>
2015-07-07 17:42:29 +0800

    contents

--- Developer/Playground/AboutSwift.playground/Contents.swift
+++ Developer/Playground/AboutSwift.playground/Contents.swift
@@ -158,3 +158,41 @@ let bDescription = b.simpleDescription
 //c.adjust()
 //let cDescription = c.simpleDescription
 
+func repeat<Item>(item: Item, times: Int) -> [Item] {
+    var result = [Item]()
+    for i in 0..<times {
+        result.append(item)
+    }
+    return result
+}
+repeat("knock", 4)
+
+//Reimplement the Swift standard library's optional type
+enum OptionalValue<T> {
+    case None
+    case Some(T)
+}
+var possibleInteger: OptionalValue<Int> = .None
+possibleInteger = .Some(100)
+
+func anyCommonElements <T, U where T: SequenceType, U:SequenceType, T.Generator.Element: Equatable,
+    T.Generator.Element == U.Generator.Element> (lhs:T, rhs:U) -> Bool {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+}
+anyCommonElements([1, 2, 3], [3])
+
+func anyCommonElements2 <T, U where T: SequenceType,
+                            U:SequenceType,
+                            T.Generator.Element: Equatable,
+                            T.Generator.Element == U.Generator.Element>(lhs:T,rhs:U) -> Bool {
+        return false
+}
+
+
--- Developer/Playground/Methods.playground/Contents.swift
+++ Developer/Playground/Methods.playground/Contents.swift
@@ -1,3 +1,146 @@
 import UIKit
 
 
+class Counter {
+    var count = 0
+    func increment() {
+        count++
+    }
+    func incrementBy(amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+
+let counter = Counter()
+// the initial counter value is 0
+counter.increment()
+// the counter's value is now 1
+counter.incrementBy(5)
+// the counter's value is now 6
+counter.reset()
+// the counter's value is now 0
+
+//: Local and External Parameter Names for Methods
+class AnohterCounter {
+    var count: Int = 0
+    func incrementBy(amount: Int, numberOfTimes: Int) {
+        count += amount * numberOfTimes
+    }
+    func incrementBy2(amount: Int, #numberOfTimes: Int) {
+        count += amount * numberOfTimes
+    }
+}
+
+let anotherCounter = AnohterCounter()
+anotherCounter.incrementBy(5, numberOfTimes: 3)
+anotherCounter
+anotherCounter.incrementBy2(5, numberOfTimes: 2)
+anotherCounter
+// anotherCounter value is now 15
+
+struct Point {
+    var x = 0.0, y = 0.0
+    func isToTheRightOfX(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+let somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOfX(1.0) {
+    println("This point is to the right of the line where x == 1.0")
+}
+// prints "This point is to the rigth of the line whtere x == 1.0
+
+struct Point2 {
+    var x = 0.0, y = 0.0
+    mutating func moveByX(deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+var somePoint2 = Point2(x: 1.0, y: 1.0)
+somePoint2.moveByX(2.0, y: 3.0)
+println("The point is now at (\(somePoint2.x),\(somePoint2.y))")
+
+//: Assigning to self Within a Mutating Method
+struct Point3 {
+    var x = 0.0, y = 0.0
+    mutating func moveByX(deltaX: Double, y deltaY: Double) {
+        self = Point3(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+enum TriStateSwitch {
+    case Off, Low, High
+    mutating func next() {
+        switch self {
+        case .Off:
+            self = Low
+        case .Low:
+            self = High
+        case .High:
+            self = Off
+        }
+    }
+}
+var ovenLight = TriStateSwitch.Low
+ovenLight.next()
+// ovenLight is now equal to .High
+ovenLight.next()
+// ovenLight is now equal to .Off
+
+
+class  SomeClass {
+    class func sometypeMethod() {
+        // type method implementation goes here
+    }
+}
+SomeClass.sometypeMethod()
+
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    static func unlockLevel(level: Int) {
+        if level > highestUnlockedLevel {
+            highestUnlockedLevel = level
+        }
+    }
+    static func levelIsUnlocked(level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    var currentLevel = 1
+    mutating func advanceToLevel(level: Int) -> Bool {
+        if LevelTracker.levelIsUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func completedLevel(level: Int) {
+        LevelTracker.unlockLevel(level + 1)
+        tracker.advanceToLevel(level + 1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+var player = Player(name: "Argyrios")
+player.completedLevel(1)
+println("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+// prints "highest unlocked level is now 2"
+
+player = Player(name: "Beto")
+if player.tracker.advanceToLevel(6) {
+    println("player is now on level 6")
+} else {
+    println("level 6 has not yet been unlocked")
+}
+// prints "level 6 has not yet been unlocked"
+
